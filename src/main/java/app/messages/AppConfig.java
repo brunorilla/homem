c15825e83a72e6ea365fb9a28d5f932abe934a0c
Bruno Rilla <brilla@Lenovo.com>
2020-12-26 21:05:15 -0300

    adding springBoot

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -10,13 +10,4 @@ import javax.management.MXBean;
 @ComponentScan("app.messages")
 
 public class AppConfig {
-    @Bean
-    public MessageRepository messageRepository() {
-        return new MessageRepository();
-    }
-
-    @Bean
-    MessageService messageService(){
-        return new MessageService(messageRepository());
-    }
 }
--- src/main/java/app/messages/MessageService.java
+++ src/main/java/app/messages/MessageService.java
@@ -1,8 +1,11 @@
 package app.messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MessageService {
     private MessageRepository repository;
-
+    @Autowired
     public MessageService(MessageRepository repository){
         this.repository = repository;
     }

33608e3c0565027a50cde4cdb0765e453eaf7201
Bruno Rilla <brilla@Lenovo.com>
2020-12-27 01:28:47 -0300

    message controller added

--- src/main/java/app/messages/MessageController.java
+++ src/main/java/app/messages/MessageController.java
@@ -11,7 +11,7 @@ public class MessageController {
     @GetMapping("/welcome")
     @ResponseBody
     public String welcome(){
-        return "Hello, Welcome to Spring Boot!";
+        return "First Controller Added!";
     }
 
 }

ece28a6948ad511ccc347a728cb3c2a4f3c63f16
Bruno Rilla <brilla@Lenovo.com>
2020-12-27 14:28:35 -0300

    adding filters

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -3,11 +3,23 @@ package app.messages;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 import javax.management.MXBean;
+import java.util.Arrays;
 
 @Configuration
 @ComponentScan("app.messages")
 
 public class AppConfig {
+    @Bean
+    public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean(){
+        FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
+        AuditingFilter filter = new AuditingFilter();
+        registration.setFilter(filter);
+        registration.setOrder(Integer.MAX_VALUE);
+        registration.setUrlPatterns(Arrays.asList("/messages/*"));
+        return registration;
+    }
 }
--- src/main/java/app/messages/MessageController.java
+++ src/main/java/app/messages/MessageController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
     @GetMapping("/welcome")
-    @ResponseBody
-    public String welcome(){
-        return "First Controller Added!";
+    public String welcome(Model model){
+        model.addAttribute("message","Hello, Welcome!");
+        return "welcome";
     }
 
 }
